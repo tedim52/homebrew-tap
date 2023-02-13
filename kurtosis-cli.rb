76e4e193af9009997e856c02bee0a4b06db953f1
Tedi Mitiku <tedi.m52@gmail.com>
2023-02-13 10:23:39 -0500

    Automated formula update for the CLI, version 0.66.3

--- kurtosis-cli.rb
+++ kurtosis-cli.rb
@@ -5,7 +5,7 @@
 class KurtosisCli < Formula
   desc "CLI for managing distro7122 environments"
   homepage "https://www.kurtosistech.com"
-  version "0.65.1"
+  version "0.65.2"
 
   on_macos do
     if Hardware::CPU.arm?

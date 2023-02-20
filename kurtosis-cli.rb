c5dfb50f78f7bc8870dea9785f108bb64c85265f
Tedi Mitiku <tedi.m52@gmail.com>
2023-02-20 09:55:11 -0500

    0.67.18

--- kurtosis-cli.rb
+++ kurtosis-cli.rb
@@ -5,7 +5,7 @@
 class KurtosisCli < Formula
   desc "CLI for managing Kurtosis environments."
   homepage "https://www.kurtosistech.com"
-  version "0.67.17"
+  version "0.67.18"
 
   on_macos do
     if Hardware::CPU.arm?
